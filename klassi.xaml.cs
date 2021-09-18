931672b134d77b1566c9fcf3969f5fccdf4ab549
xxxpain99 <84181647+xxxpain99@users.noreply.github.com>
2021-09-18 10:26:35 +0500

    Add files via upload

---
+++ klassi.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp10.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для klassi.xaml
+    /// </summary>
+    public partial class klassi : Page
+    {
+        public klassi()
+        {
+            InitializeComponent();
+            LoadGroupList();
+        }
+
+        private void LoadGroupList()
+        {
+            db.base10Entities connection = new db.base10Entities();
+            var groupList = connection.Класс.ToList();
+
+            foreach (var group in groupList)
+            {
+                ComboBoxGroup.Items.Add(group.Номер);
+            }
+
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ComboBoxGroup.SelectedIndex == -1)
+            {
+
+            }
+            int g = (int)ComboBoxGroup.SelectedItem;
+        }
+    }
+}
---
+++ ucheniki.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp10.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ucheniki.xaml
+    /// </summary>
+    public partial class ucheniki : Page
+    {
+        public ucheniki()
+        {
+            InitializeComponent();
+       
+        }
+        private void LoadGroupList()
+        {
+        
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int groupNumber = int.Parse(TextBoxGroup.Text);
+
+            //sozdat podklyuchenie k BD
+            db.base10Entities connection = new db.base10Entities();
+            db.Класс isExists = connection.Класс.Where(g => g.Номер == groupNumber).FirstOrDefault();
+            if (isExists != null)
+            {
+                MessageBox.Show("Класс c таким номером уже существует");
+                return;
+            }
+            db.Класс group = new db.Класс();
+            group.Номер = groupNumber;
+            connection.Класс.Add(group);
+
+
+            int result = connection.SaveChanges();
+            if (result == 1)
+            {
+                TextBoxGroup.Text = "";
+                MessageBox.Show("Класс успешно создан");
+            }
+        }
+    }
+    
+}
